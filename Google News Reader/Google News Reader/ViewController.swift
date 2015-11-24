fa1404b9acf319708f514dc976f4c263bc6f8d4c
Josh Lieberman <josh.lieberman92@gmail.com>
2015-11-23 21:12:40 -0500

    initial project setup

---
+++ Google News Reader/Google News Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Google News Reader
+//
+//  Created by Josh Lieberman on 11/23/15.
+//  Copyright © 2015 Josh Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
